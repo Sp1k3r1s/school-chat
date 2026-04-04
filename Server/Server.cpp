0cbeb70263f84afcbb92707336713951bbac789f
K4rtas <120280650+Sp1k3r1s@users.noreply.github.com>
2026-04-04 09:06:50 -0700

    Server broadcasting v1
    Added first version of broadcasting from the server, added threading to the server, so it supports multiple clients at the same time.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,30 +1,31 @@
-﻿#include <asio.hpp>
+#include <asio.hpp>
 #include <asio/ssl.hpp>
 
 #include <iostream>
 #include <memory>
+#include <vector>
+#include <set>
+#include <mutex>
+
+class server;
 
 class session : public std::enable_shared_from_this<session> {
 	asio::ssl::stream<asio::ip::tcp::socket> _socket;
 	char _data[1024];
+	std::weak_ptr<server> _server;
+
+	void do_write();
 
 	void do_read() {
 		auto self(shared_from_this());
 		memset(_data, 0x0, 1024);
 		_socket.async_read_some(asio::buffer(_data, 1024),
-			[this, self](std::error_code ec, std::size_t length) {
+			[self](std::error_code ec, std::size_t length) {
 				if (!ec) {
-					do_write();
+					self->do_write();
 				}
-			});
-	}
-
-	void do_write() {
-		auto self(shared_from_this());
-		_socket.async_write_some(asio::buffer(_data, 1024),
-			[this, self](std::error_code ec, std::size_t length) {
-				if (!ec) {
-					do_read();
+				else {
+					self->stop();
 				}
 			});
 	}
@@ -32,43 +33,63 @@ class session : public std::enable_shared_from_this<session> {
 	void do_handshake() {
 		auto self(shared_from_this());
 		_socket.async_handshake(asio::ssl::stream_base::server,
-			[this, self](std::error_code ec) {
+			[self](std::error_code ec) {
 				if (!ec) {
 					std::cout << "Client handshare success!\n";
-					do_read();
+					self->do_read();
+				}
+				else {
+					self->stop();
 				}
 			});
 	}
-	
+
+	void stop();
+
 public:
-	session(asio::ssl::stream<asio::ip::tcp::socket> socket) : _socket(std::move(socket)) {}
+	session(asio::ssl::stream<asio::ip::tcp::socket> socket, std::shared_ptr<server> server) :
+		_socket(std::move(socket)), _server(std::move(server)) {
+		memset(_data, 0x0, 1024);
+	}
+
+	void send(const std::string& message);
 
 	void start() {
 		do_handshake();
 	}
 };
 
-class server {
+class server : public std::enable_shared_from_this<server> {
 	asio::ip::tcp::acceptor _acceptor;
 	asio::ssl::context _ssl_context;
+
+	std::set<std::shared_ptr<session>> _sessions;
+	std::mutex _sessions_mutex;
 
 	void do_accept() {
 		_acceptor.async_accept(
 			[this](std::error_code ec, asio::ip::tcp::socket socket) {
 				if (!ec) {
+					auto self = shared_from_this();
+					auto s = std::make_shared<session>(
+						asio::ssl::stream<asio::ip::tcp::socket>(std::move(socket), _ssl_context),
+						self
+					);
+
+					std::lock_guard<std::mutex> lock(_sessions_mutex);
+					_sessions.insert(s);
+
+					s->start();
 					std::cout << "New client connected! \n";
-					std::make_shared<session>(
-						asio::ssl::stream<asio::ip::tcp::socket>(std::move(socket), _ssl_context)
-					)->start();
 				}
 				do_accept();
 			});
 	}
 
 public:
-	server(asio::io_context& io_context, short port) : 
+	server(asio::io_context& io_context, short port) :
 		_acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)),
-		_ssl_context(asio::ssl::context::sslv23) {
+		_ssl_context(asio::ssl::context::tls_server) {
 
 		_ssl_context.set_options(
 			asio::ssl::context::default_workarounds
@@ -80,13 +101,76 @@ public:
 
 		do_accept();
 	}
+
+	void broadcast(const std::string& message) {
+		std::lock_guard<std::mutex> lock(_sessions_mutex);
+		for (auto& session : _sessions) {
+			if (session) {
+				session->send(message);
+			}
+		}
+	}
+
+	void remove_session(std::shared_ptr<session> s) {
+		std::lock_guard<std::mutex> lock(_sessions_mutex);
+		_sessions.erase(s);
+	}
 };
+
+void session::send(const std::string& message) {
+	auto self(shared_from_this());
+	asio::async_write(_socket, asio::buffer(message),
+		[self](const asio::error_code& e, size_t lenght) {
+			if (e) self->stop();
+		}
+	);
+}
+
+void session::stop() {
+	auto server = _server.lock();
+	if (server) {
+		server->remove_session(shared_from_this());
+	}
+}
+
+void session::do_write() {
+	auto self(shared_from_this());
+	_socket.async_write_some(asio::buffer(_data, 1024),
+		[self](std::error_code ec, std::size_t length) {
+			if (!ec) {
+				auto serverPtr = self->_server.lock();
+				if (serverPtr) {
+					serverPtr->broadcast(std::string(self->_data, length)); 
+				}
+				self->do_read();
+			}
+			else {
+				self->stop();
+			}
+		});
+}
 
 int main()
 {
 	try {
 		asio::io_context io;
-		server s(io, 1337);
+		auto s = std::make_shared<server>(io, 1337);
+
+		std::vector<std::thread> threadpool;
+		unsigned int thread_count = std::thread::hardware_concurrency();
+		if (thread_count == 0) thread_count = 4;
+
+		for (unsigned int i = 0; i < thread_count; i++) {
+			threadpool.emplace_back(
+				[&io]() {
+					io.run();
+				}
+			);
+		}
+
+
+		for (std::thread& t : threadpool) t.join();
+
 		io.run();
 	}
 	catch (const std::exception& e) {
